997cdc1f9b13779055245a2b4b13bc4f0ae3c438
claudia.ramos <claudia.ramos@fieb.org.br>
2026-08-13 18:00:47 -0300

    Add commands to read water sensor

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <chrono>
 #include <thread>
 #include <string>
+#include <unistd.h>
+#include <stdio.h>
 
 #include "i2c_device_ros/i2c_device.hpp"
 
@@ -19,22 +21,18 @@ int main(int argc, char* argv[])
   // std::cout << "Temp: " << temperature << " Hum: " << humidity << std::endl;
 
   int id = std::stoi(argv[1], nullptr, 16);
+  uint8_t config[2] = {0xC3, 0x83};
   I2CDevice i2c;
   i2c.openI2CBus("/dev/i2c-1", id);
-  uint8_t data[6];
-  i2c.readBytes(0xFD, 2, data);
-  uint16_t water = (static_cast<uint16_t>(data[0]) << 8) | data[1];
+  // Write on config register 01, gain of 4.096V
+  i2c.writeBytes(0x01, 2, config);
+  usleep(100);
 
-  while (true){
-    if (water > 32767){
-      water -= 65536;
-    }
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    // uint16_t raw_humidity = (static_cast<uint16_t>(data[3]) << 8) | data[4];
-    double out = water * 4.096 / 32768.0;
-    // double humidity = -6.0 + 125.0 * raw_humidity / 65535.0;
-    // std::cout << "Temp: " << temperature << " Hum: " << humidity << std::endl;
-    std::cout << "Water Level: " << out << std::endl;
-  }
+  uint8_t raw[2];
+  // Read conversion register
+  i2c.readBytes(0x00, 2, raw);
+  uint16_t result = (static_cast<uint16_t>(raw[0]) << 8) | raw[1];
+  double out = result * 4.096 / 32768.0;
+  std::cout << "Voltage: " << result << std::endl;
   return 0;
 }
